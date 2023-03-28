ef7d9418eca581e3cf6154adbe7472ea17514376
hmuaz <muazsenol1@gmail.com>
2023-03-28 15:25:38 +0300

    Player Movement

---
+++ Assets/_Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 10f;
+    private bool isWalking = false;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;   
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;    
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;   
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.forward = Vector3.Lerp(transform.position, moveDirection, Time.deltaTime * rotateSpeed);
+
+        isWalking = moveDirection != Vector3.zero;
+        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}

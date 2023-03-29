e49d0350fedb24fe0440b87d83219a4065c70f66
hmuaz <muazsenol1@gmail.com>
2023-03-29 17:17:11 +0300

    Move KitchenObject to Next Counter

--- Assets/_Assets/Scripts/ClearCounter.cs
+++ Assets/_Assets/Scripts/ClearCounter.cs
@@ -6,11 +6,41 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if(testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if(kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
     public void Interact()
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if(kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+        
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
     }
 }
--- Assets/_Assets/Scripts/KitchenObject.cs
+++ Assets/_Assets/Scripts/KitchenObject.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
+    private ClearCounter clearCounter;
     public KitchenObjectSO GetKitchenObjectSO() { return kitchenObjectSO; }
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetClearCounter() { return clearCounter; }
 }

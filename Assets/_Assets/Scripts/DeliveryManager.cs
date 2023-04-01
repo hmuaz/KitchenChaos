f6e4cdee2677648f8610f05763a25da89c612169
hmuaz <muazsenol1@gmail.com>
2023-04-02 02:30:54 +0300

    Game Over

--- Assets/_Assets/Scripts/DeliveryManager.cs
+++ Assets/_Assets/Scripts/DeliveryManager.cs
@@ -21,6 +21,8 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
 
+    private int successfulRecipesAmount;
+
     private void Awake()
     {
         waitingRecipeSOList = new List<RecipeSO>();
@@ -75,6 +77,8 @@ public class DeliveryManager : MonoBehaviour
                 if(plateContentsMatchesRecipe)
                 {
                     //Player delivered the correct recipe
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -92,5 +96,10 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }

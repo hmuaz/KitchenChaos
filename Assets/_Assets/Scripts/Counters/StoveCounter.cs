eb22c9281eddec145fdd5f7c66222d42709901b5
hmuaz <muazsenol1@gmail.com>
2023-04-04 18:13:41 +0300

    Polish - ProgressBarFlash

--- Assets/_Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/_Assets/Scripts/Counters/StoveCounter.cs
@@ -199,4 +199,8 @@ public class StoveCounter : BaseCounter, IHasProgress
         }
         return null;
     }
+    public bool IsFried()
+    {
+        return state == State.Fried;
+    }
 }
--- Assets/_Assets/Scripts/SoundManager.cs
+++ Assets/_Assets/Scripts/SoundManager.cs
@@ -80,6 +80,10 @@ public class SoundManager : MonoBehaviour
     {
         PlaySound(audioClipRefsSO.warning, Vector3.zero);
     }
+    public void PlayWarningSound(Vector3 position)
+    {
+        PlaySound(audioClipRefsSO.warning, position);
+    }
 
     public void ChangeVolume()
     {

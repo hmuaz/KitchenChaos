d71b97a1de5016fabb46cb333ec131d1fb0451fc
hmuaz <muazsenol1@gmail.com>
2023-04-04 17:19:30 +0300

    Polish - Tutorial UI

--- Assets/_Assets/Scripts/GameInput.cs
+++ Assets/_Assets/Scripts/GameInput.cs
@@ -18,6 +18,8 @@ public class GameInput : MonoBehaviour
 
     public event EventHandler OnPauseAction;
 
+    public event EventHandler OnBindingRebind;
+
     public enum Binding
     {
         Move_Up,
@@ -179,6 +181,8 @@ public class GameInput : MonoBehaviour
 
                 PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
                 PlayerPrefs.Save();
+
+                OnBindingRebind?.Invoke(this, EventArgs.Empty);
             })
             .Start();
     
